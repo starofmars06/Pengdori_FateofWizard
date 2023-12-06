302386b6b0cfd7d3db7e68d19e665b988cd116e2
starofmars06 <starofmars06@sookmyung.ac.kr>
2023-12-06 14:46:13 +0900

    마법 연습 손동작(4개)

--- Assets/Scripts/SpawnObjectOnClick.cs
+++ Assets/Scripts/SpawnObjectOnClick.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class SpawnObjectOnClick : MonoBehaviour
 {
     [SerializeField] private GameObject aRObject;
+    [SerializeField] private GameObject aRObject1;
+    [SerializeField] private GameObject aRObject2;
+    [SerializeField] private GameObject aRObject3;
 
     // Update is called once per frame
     void Update()
@@ -14,12 +17,29 @@ public class SpawnObjectOnClick : MonoBehaviour
         GestureInfo gestureInfo = ManomotionManager.Instance.Hand_infos[0].hand_info.gesture_info;
         ManoGestureTrigger currentGesture = gestureInfo.mano_gesture_trigger;
 
+
+        //마법 물약이 0개라면 실행되는 부분이 없어야 함.
+        
+
         if (currentGesture == ManoGestureTrigger.CLICK)
         {
             SpawnObject();
         }
+        else if(currentGesture == ManoGestureTrigger.GRAB_GESTURE)
+        {
+            SpawnObject1();
+        }
+        else if(currentGesture == ManoGestureTrigger.SWIPE_RIGHT)
+        {
+            SpawnObject2();
+        }
+        else if (currentGesture == ManoGestureTrigger.PICK)
+        {
+            SpawnObject3();
+        }
     }
 
+    //첫번째 마법
     private void SpawnObject()
     {
         ManomotionManager.Instance.ShouldCalculateSkeleton3D(true);
@@ -31,6 +51,60 @@ public class SpawnObjectOnClick : MonoBehaviour
         Vector3 jointPosition = ManoUtils.Instance.CalculateNewPositionSkeletonJointDepth(new Vector3(trackingInfo.skeleton.joints[8].x, trackingInfo.skeleton.joints[8].y, trackingInfo.skeleton.joints[8].z), depthEstimation);
 
         Instantiate(aRObject, jointPosition, Quaternion.identity);
+
+
+
+        Handheld.Vibrate();
+    }
+
+    //두번째 마법
+    private void SpawnObject1()
+    {
+        ManomotionManager.Instance.ShouldCalculateSkeleton3D(true);
+
+        TrackingInfo trackingInfo = ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info;
+
+        float depthEstimation = trackingInfo.depth_estimation;
+
+        Vector3 jointPosition = ManoUtils.Instance.CalculateNewPositionSkeletonJointDepth(new Vector3(trackingInfo.skeleton.joints[8].x, trackingInfo.skeleton.joints[8].y, trackingInfo.skeleton.joints[8].z), depthEstimation);
+
+        Instantiate(aRObject1, jointPosition, Quaternion.identity);
+
+
+
+        Handheld.Vibrate();
+    }
+
+    //세번째 마법
+    private void SpawnObject2()
+    {
+        ManomotionManager.Instance.ShouldCalculateSkeleton3D(true);
+
+        TrackingInfo trackingInfo = ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info;
+
+        float depthEstimation = trackingInfo.depth_estimation;
+
+        Vector3 jointPosition = ManoUtils.Instance.CalculateNewPositionSkeletonJointDepth(new Vector3(trackingInfo.skeleton.joints[8].x, trackingInfo.skeleton.joints[8].y, trackingInfo.skeleton.joints[8].z), depthEstimation);
+
+        Instantiate(aRObject2, jointPosition, Quaternion.identity);
+
+
+
+        Handheld.Vibrate();
+    }
+
+    //네번째 마법
+    private void SpawnObject3()
+    {
+        ManomotionManager.Instance.ShouldCalculateSkeleton3D(true);
+
+        TrackingInfo trackingInfo = ManomotionManager.Instance.Hand_infos[0].hand_info.tracking_info;
+
+        float depthEstimation = trackingInfo.depth_estimation;
+
+        Vector3 jointPosition = ManoUtils.Instance.CalculateNewPositionSkeletonJointDepth(new Vector3(trackingInfo.skeleton.joints[8].x, trackingInfo.skeleton.joints[8].y, trackingInfo.skeleton.joints[8].z), depthEstimation);
+
+        Instantiate(aRObject3, jointPosition, Quaternion.identity);
 
 
 
