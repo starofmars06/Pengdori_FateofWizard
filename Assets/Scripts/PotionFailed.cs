c0edb1acce9360b9e1999bacd9188f2494458179
comastlu <129175682+comastlu@users.noreply.github.com>
2023-12-10 01:36:26 +0900

    마법약 실패 씬 세부 내용 추가

---
+++ Assets/Scripts/PotionFailed.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PotionFailed : MonoBehaviour
+{
+    public Text result;
+    public Text touch;
+
+    IEnumerator Waittime()
+    {
+        yield return new WaitForSeconds(2f);
+
+        result.text = "물약 만들기에 실패하였습니다.";
+
+        touch.text = "터치하여 재도전";
+
+        if (Input.touchCount > 0)
+        {
+            SceneManager.LoadScene("PotionScene");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Waittime());
+    }
+}
